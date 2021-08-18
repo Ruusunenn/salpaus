fc05b719e0fcd4f7749a1a3d3273eee43374dd47
Ruusunenn <88773191+Ruusunenn@users.noreply.github.com>
2021-08-18 10:50:22 +0000

    emt

---
+++ script.js
@@ -0,0 +1,51 @@
+function laskutehtava(ekaArvo, tokaArvo){
+  return ekaArvo + tokaArvo;
+}
+
+console.log("täällä ollaan");
+
+let ika = 16; // numoreot kirjoitetaan ilman lainausmerkkejä
+ika = ika + 1; // integer (int)
+
+let ikakirjaimina = "17" //string (str)
+ikakirjaimina = ikakirjaimina + 1;
+
+console.log(ikakirjaimina);
+
+console.log("sinun ikäsi on: " + ika);
+
+let juuvaiei = true; //boolean
+
+console.log(juuvaiei);
+
+let nimi = "keijo";
+
+console.log("kirjoita isolla:" + nimi.toUpperCase());
+
+console.log("splittausharjoitus: " + nimi.split(" ")[0]);
+
+//while ja for loop
+
+for(let i=0; i<=10; i++){
+  console.log("loopin iteraatiolku on: " + i);
+}
+
+if(ika > 18 || nimi == "nico ruusunen"){
+  console.log("onnea olet täysi-ikäinen!");
+}
+else if (ika === 18 && nimi == "keijo"){
+console.log("onnea keijo, olet juuri ja juuri täysi-ikäinen!");
+}
+else{
+  console.error("kasva vielä vähän...");
+}
+
+console.log(laskutehtava(843937, 983789));
+
+
+
+/*
+monen 
+rivin
+kommentti
+*/
